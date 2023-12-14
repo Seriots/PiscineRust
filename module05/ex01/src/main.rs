8ae1c3d1a1eb5b12b8342855feaf8e10bad626cf
seriots <leo.giband@gmail.com>
2023-12-14 01:05:25 +0100

    Mod05 ex01-02

--- module05/ex01/src/main.rs
+++ module05/ex01/src/main.rs
@@ -1,8 +1,9 @@
 use std::io::{Stdout, StdoutLock, Stdin, StdinLock};
 use std::io::{Write, Read, stdin, stdout};
+use std::fs::File;
 
 
-fn to_str(buffer: &[u8]) -> String {
+pub fn to_str(buffer: &[u8]) -> String {
     let mut str: String = String::new();
     for i in buffer {
         str.push(*i as char);
@@ -11,13 +12,36 @@ fn to_str(buffer: &[u8]) -> String {
 }
 
 fn main() {
+    let mut v: Vec<File> = Vec::new();
+
+    std::env::args().skip(1).for_each(|arg| {
+        File::create(&arg).map(|file| {
+            v.push(file);
+        }).unwrap_or_else(|e| {
+            println!("error: {:?}", e);
+        });
+    });
+
     let mut buf :[u8; 4096] = [0; 4096];
 
     loop {
         match stdin().read(&mut buf) {
             Ok(0) => break,
             Ok(count) => {
-                stdout().write(&buf[..count]).unwrap();
+                match stdout().write_all(&buf[..count]) {
+                    Ok(_) => {},
+                    Err(e) => {
+                        println!("error: {:?}", e);
+                    }
+                };
+                for each in &mut v {
+                    match each.write_all(&buf[..count]) {
+                        Ok(_) => {},
+                        Err(e) => {
+                            println!("error: {:?}", e);
+                        }
+                    };
+                }
             },
             Err(e) => {
                 println!("error: {:?}", e);
--- module05/ex02/src/main.rs
+++ module05/ex02/src/main.rs
@@ -1,3 +1,47 @@
+use std::fs::metadata;
+
+fn get_directory_size(path: &str) -> u64 {
+    let mut size = 0;
+    for each in std::fs::read_dir(path).unwrap() {
+        let meta = each.as_ref().unwrap().metadata().unwrap();
+        size += meta.len();
+        if meta.is_dir() {
+            size += get_directory_size(&each.unwrap().path().to_str().unwrap());
+        }
+
+    }
+    size
+}
+
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() != 2 {
+        eprintln!("Usage: ex02 <input>");
+        return ;
+    }
+
+    let file = std::env::args().nth(1).unwrap();
+
+    let meta = match metadata(&file) {
+        Ok(meta) => meta,
+        Err(e) => {
+            eprintln!("error: {:?}", e);
+            return ;
+        }
+    };
+
+    let mut size = 0;
+    if meta.is_file() {
+        size = meta.len();
+    } else if meta.is_dir() {
+        size = get_directory_size(&file);
+    } else {
+        println!("{} is something else", file);
+    }
+
+    match size {
+        0..=999 => println!("{size} bytes"),
+        1000..=999999 => println!("{:.2} kilobytes", (size as f64)/1000.0),
+        1000000..=999999999 => println!("{:.2} megabytes", (size as f64)/1000000.0),
+        _ => println!("{:.2} gigabytes", (size as f64)/1000000000.0),
+    }
 }
