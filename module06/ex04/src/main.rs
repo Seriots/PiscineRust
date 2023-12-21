081c6e27d1895b521a5b1572e5f554df1408b1c4
seriots <leo.giband@gmail.com>
2023-12-21 12:09:33 +0100

    Ex05

--- module06/ex04/src/main.rs
+++ module06/ex04/src/main.rs
@@ -1,3 +1,38 @@
+
+use  std::sync::atomic::AtomicU8;
+use  std::sync::atomic::Ordering;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Unique(u8);
+
+impl Unique {
+    fn new_unique() -> u8 {
+        static COUNTER: AtomicU8 = AtomicU8::new(0);
+        COUNTER.fetch_add(1, Ordering::Relaxed)
+    }
+    pub fn new() -> Self {
+        Unique(Unique::new_unique())
+    }
+}
+
+impl Clone for Unique {
+    fn clone(&self) -> Self {
+        Unique::new()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Unique::new();
+    let b = Unique::new();
+    let c = Unique::new();
+
+    println!("{a:?}");
+    println!("{b:?}");
+    println!("{c:?}");
+
+    let d = a.clone();
+    let e = c.clone();
+
+    println!("{d:?}");
+    println!("{e:?}");
 }
--- module06/ex05/src/main.rs
+++ module06/ex05/src/main.rs
@@ -1,3 +1,67 @@
+use rayon::prelude::*;
+
+enum ArgsError {
+    InvalidUsage,
+    InvalidNumber,
+}
+
+fn get_input() -> Result<i32, ArgsError> {
+    let args = match std::env::args().len() {
+        2 => std::env::args().nth(1).unwrap(),
+        _ => {
+            return Err(ArgsError::InvalidUsage);
+        }
+    };
+
+    match args.parse::<i32>() {
+        Ok(n) => return Ok(n),
+        Err(_) => {
+            return Err(ArgsError::InvalidNumber);
+        }
+    };
+}
+
+fn without_rayon(n: i32) {
+    println!("---Without rayon--");
+    let time = std::time::Instant::now();
+    let pi = (0..n).into_iter().map(|_| {
+        let x = ftkit::random_number(0..10000000) as f64 / 10000000.0;
+        let y = ftkit::random_number(0..10000000) as f64 / 10000000.0;
+        if x * x + y * y <= 1.0 {
+            1
+        } else {
+            0
+        }
+    }).sum::<i32>() as f64 / n as f64 * 4.0;
+    println!("n = {}", pi);
+    println!("time = {}ms", time.elapsed().as_millis());
+}
+
+fn with_rayon(n: i32) {
+    println!("\n----With rayon----");
+    let time = std::time::Instant::now();
+    let pi = (0..n).into_par_iter().map(|_| {
+        let x = ftkit::random_number(0..10000000) as f64 / 10000000.0;
+        let y = ftkit::random_number(0..10000000) as f64 / 10000000.0;
+        if x * x + y * y <= 1.0 {
+            1
+        } else {
+            0
+        }
+    }).sum::<i32>() as f64 / n as f64 * 4.0;
+    println!("n = {}", pi);
+    println!("time = {}ms", time.elapsed().as_millis());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = match get_input() {
+        Ok(n) => n,
+        Err(err) => match err {
+            ArgsError::InvalidUsage => return eprintln!("Error: Invalid usage"),
+            ArgsError::InvalidNumber => return eprintln!("Error: Invalid number"),
+        }
+    };
+    without_rayon(n);
+    with_rayon(n);
+
 }
